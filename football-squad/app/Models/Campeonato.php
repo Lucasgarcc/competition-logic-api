629db077ea3d9d8abfe14e1587785fb5f4d63b3e
Lucas Garcia <luuccasgarciia@gmail.com>
2026-05-08 17:07:20 -0300

    feat(model): implementa model Campeonato com relacionamentos de partidas e vencedor

--- football-squad/app/Models/Campeonato.php
+++ football-squad/app/Models/Campeonato.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Campeonato extends Model
 {
     use HasFactory;
+    
+    protected $fillable = ['nome', 'vencedor_id'];
+
+    /**
+     * Relacionamento: Um campeonato tem muitas partidas.
+     */
+    public function partidas() {
+        return $this->hasMany(Partidas::class);
+    }
+
+    /**
+     * Relacionamento: Um campeonato tem um vencedor (time vencedor) 
+     */
+    public function vencedor() {
+        return $this->hasMany(Time::class, 'vencedor_id');
+    }
 }
