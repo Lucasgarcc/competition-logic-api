09086d407a9ee7faad40ee400c963075a4a74ae1
Lucas Garcia <luuccasgarciia@gmail.com>
2026-05-08 17:23:22 -0300

    feat(model): implementa model Partida com as chaves estrangeiras para mandante e visitante

--- football-squad/app/Models/Partida.php
+++ football-squad/app/Models/Partida.php
@@ -8,4 +8,34 @@ use Illuminate\Database\Eloquent\Model;
 class Partida extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'campeonato_id', 
+        'time_mandante_id', 
+        'time_visitante_id', 
+        'gols_mandante', 
+        'gols_visitante', 
+        'fase'//ex:'quartas', 'semi', 'final'
+    ]
+
+    /**
+     * Relacionamento: A partida pertence a um campeonato.
+     */
+    public function campeonato() {
+        return $this->belongsTo(Campeonato::class);
+    }
+
+    /**
+     * Relacionamento: O time que joga em casa (Mandante).
+     */
+    public function mandante() {
+        return $this->belongsTo(Time::class, 'time_mandante_id');
+    }
+
+    /**
+     * Relacionamente: O time que joga fora (Visitante). 
+     */
+    public function visitante() {
+        return $this->belongsTo(Time::class, 'time_visitante_id');
+    }
 }
