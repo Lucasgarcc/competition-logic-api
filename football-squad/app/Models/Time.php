7a2a00fb34e10ea8ce991b76954318fb2667b76c
Lucas Garcia <luuccasgarciia@gmail.com>
2026-05-08 16:55:05 -0300

    feat(model): implementa o relacionamentos de partidas e jogadores em Time

--- football-squad/app/Models/Time.php
+++ football-squad/app/Models/Time.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Time extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['nome']; 
+
+    public function jogadores() {
+        return $this->hasMany(Jogador::class);
+    }
+
+    // Útil para calcular o desempate: pontos acumulados (gols feitos - sofridos)
+    public function partidasComoMandante() {
+        return $this->hasMany(Partida::class, 'time_mandante_id');
+    }
+
+    public function partidasComoVisitante() {
+        return $this->hasMany(Partida::class, 'time_visitante_id');
+    }
 }
