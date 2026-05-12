880210044d2bea302af107fd24dd734781874cf5
Lucas Garcia <luuccasgarciia@gmail.com>
2026-05-12 19:32:08 -0300

    feat: define as views do layout e rotas

--- football-squad/routes/web.php
+++ football-squad/routes/web.php
@@ -4,7 +4,23 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CampeonatoController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('app');
 });
 
-Route::get('/simular-partida',[CampeonatoController::class, 'simularPartida']);
+// Rota para Campeonatos (dentro de resources/views/layouts/campeonatos.blade.php)
+Route::get('/campeonatos', function () {
+    return view('layouts.campeonatos');
+});
+
+// Rota para Estatísticas (dentro de resources/views/componentes/estatisticas.blade.php)
+Route::get('/estatisticas', function () {
+    return view('components.estatisticas');
+});
+
+/** Rest Full */
+Route::post('/simular-partida',[CampeonatoController::class, 'simularPartida']);
+Route::get('/simular-partida-teste',[CampeonatoController::class, 'simularPartida']);
+Route::get('/campeonato/status-atual',[CampeonatoController::class, 'statusAtual']);
+
+Route::get('/campeonatos', [CampeonatoController::class, 'index']);
+Route::get('/estatisticas-globais', [CampeonatoController::class, 'estatisticasGlobais']);
